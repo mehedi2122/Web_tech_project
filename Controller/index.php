d1c967bf89b93c590d1116126a3fc6a2d24e811a
mehedi2122 <patwarymehedi13@gmail.com>
2026-01-03 00:19:01 +0600

    html and css

---
+++ Controller/index.php
@@ -0,0 +1,114 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <label class="logo">W-school</label>
+    
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Contact</a></li>
+        <li><a href="">Admission</a></li>
+        <li><a href="" class="btn_login">Login</a></li>
+    </ul></nav>
+    <div class="section_1">
+        <label class ="img_text">We teach student with care</label>
+        <img class="main_img" src="img_2.jpg">    
+        </div>
+        <div class="container">
+            
+                     <img class="welcome_img" src="img_3.jfif"> 
+                    <div class="welcome_text">
+                     <h1>Welcome to W-school</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+</div>
+<center>
+    <h1>Our Teachers</h1>
+</center>
+<section class="teachers">
+
+    <div class="teacher-row">
+
+        <div class="teacher-card">
+            <img src="teacher_1.png" alt="Teacher 1">
+            <h3>Mr. Rahim Ahmed</h3>
+            <p>Data ScienceTeacher</p>
+        </div>
+
+        <div class="teacher-card">
+            <img src="teacher_2.jfif" alt="Teacher 2">
+            <h3>Ms. Nusrat Jahan</h3>
+            <p>Machine LearningTeacher</p>
+        </div>
+
+        <div class="teacher-card">
+            <img src="teacher_3.jfif" alt="Teacher 3">
+            <h3>Mr. Karim Hossain</h3>
+            <p>SComputer Vision Teacher</p>
+        </div>
+
+    </div>
+</section>
+
+        <center>
+    <h1>Our Courses</h1>
+</center>
+
+<section class="courses">
+
+    <div class="course-row">
+
+        <div class="course-card">
+            <img src="ds.webp" alt="Course 1">
+            <h3>Data Science</h3>
+            
+        </div>
+
+        <div class="course-card">
+            <img src="ml.webp" alt="Course 2">
+            <h3>Machine Learning</h3>
+            
+        </div>
+
+        <div class="course-card">
+            <img src="cv.jfif" alt="Course 3">
+            <h3>Computer Vision</h3>
+            
+        </div>
+
+    </div>
+</section>
+
+<center>
+    <h1>Admission Form</h1>
+    <form>
+        <div>
+            <label class="label_text">Name</label>
+            <input class="input_deg" type="text" name="">
+        </div>
+        <div>
+            <label class="label_text">Email</label>
+            <input class="input_deg" type="email" name="" >
+        </div>
+        <div>
+            <label class="label_text">Phone</label>
+            <input class="input_deg" type="text" name="" >
+        </div>
+        <div>
+            <label class="label_text">Message</label>
+            <textarea></textarea>
+        </div>
+        <div>
+            <input type="submit">
+        </div>
+    </form>
+</center>
+</body>
+</html>
